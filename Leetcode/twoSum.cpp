08f29ad25d705c6eb651bd40eb1e66b64432c9c1
Muhammad Amir Aslam <amiraslam1234@users.noreply.github.com>
2023-10-09 10:47:04 +0500

    leetcode(Two Sum)

---
+++ Leetcode/twoSum.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>sums;
+        for (int i = 0; i < nums.size()-1; i++)
+        {
+            for (int j = i+1; j < nums.size(); j++)
+            {
+                cout<<"a";
+                if((nums[i]+nums[j])==target){
+                    sums.push_back(i);
+                    sums.push_back(j);
+                return sums;
+                }
+            }
+            
+        }
+        return sums;
+    }
+};
+int main(){
+    vector<int>nums = {2,11,7,15};
+    Solution obj;
+    vector<int>sums=obj.twoSum(nums,9);
+    cout<<sums[0];
+    cout<<sums[1];
+    return 0;
+}
