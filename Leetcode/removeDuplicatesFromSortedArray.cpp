e476812fadb54f55193b2c370fcc0f952a11cf85
Muhammad Amir Aslam <amiraslam1234@users.noreply.github.com>
2023-10-25 01:45:33 +0500

    LeetCode(Remove duplicate form sorted array)

---
+++ Leetcode/removeDuplicatesFromSortedArray.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        std::unique(nums.begin(),nums.end());
+        int k = 0;
+        int i = 0;
+        for(int j =0;j<nums.size();j++){
+            cout<<nums[j];
+        }
+        while(true){
+            if(nums[i]!=nums[i+1]||(i+1)==(nums.size()-1)){
+                k++;
+            }
+            else{
+                break;
+
+            }
+            
+            i++;
+        }
+       
+        return k;
+    }
+};
+int main(){
+    Solution obj;
+    vector<int>nums = {1,1,2};
+    // vector<int>nums = {0,0,1,1,1,2,2,3,3,4};
+    obj.removeDuplicates(nums);
+    return 0;
+}
